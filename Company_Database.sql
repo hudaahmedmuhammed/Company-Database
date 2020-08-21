1bfa049fcc22ea93239681145efa6b9cc0679842
Huda Ahmed Abd-almajeed <hoda.ah2@std.commerce.asu.edu.eg>
2020-08-21 18:12:51 +0200

    Update Company_Database.sql

--- Company_Database.sql
+++ Company_Database.sql
@@ -206,3 +206,17 @@ FROM emploee;
 SELECT SUM(salary)
 FROM emploee;
 
+-- Find out how many males and females there are
+SELECT COUNT(sex), sex
+FROM employee
+GROUP BY sex
+
+-- Find the total sales of each salesman
+SELECT SUM(total_sales), emp_id
+FROM works_with
+GROUP BY client_id;
+
+-- Find the total amount of money spent by each client
+SELECT SUM(total_sales), client_id
+FROM works_with
+GROUP BY client_id;
